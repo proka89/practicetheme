b57e4d713fa04332605d107d63865660e8821857
proka89 <vukovicjovan89@gmail.com>
2018-01-15 10:34:29 +0100

    fix2

--- page-7.php
+++ page-7.php
@@ -1,12 +1,12 @@
 <!-- Custom template for about me page, 7 is the page ID -->
 <?php get_header(); ?>
-	<?php
-		if (have_posts()):
-			while(have_posts()): the_post(); ?>
-				<p><?php the_content(); ?></p>
+  <?php
+  if (have_posts()):
+  while(have_posts()): the_post(); ?>
+  <p><?php the_content(); ?></p>
         <h3><?php the_title(); ?></h3>
-				<hr>
-			<?php endwhile;
-		endif;
-	 ?>
+  <hr>
+  <?php endwhile;
+  endif;
+   ?>
 <?php get_footer(); ?>
--- page-notitle.php
+++ page-notitle.php
@@ -4,14 +4,14 @@
   Template Name: Page No Title
 */
 get_header(); ?>
-	<?php
-		if (have_posts()):
-			while(have_posts()): the_post(); ?>
-				<h1>This is static title</h1>
-				<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
-				<p><?php the_content(); ?></p>
-				<hr>
-			<?php endwhile;
-		endif;
-	 ?>
+  <?php
+  if (have_posts()):
+  while(have_posts()): the_post(); ?>
+  <h1>This is static title</h1>
+  <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+  <p><?php the_content(); ?></p>
+  <hr>
+  <?php endwhile;
+  endif;
+   ?>
 <?php get_footer(); ?>
