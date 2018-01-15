b73c48fa9e2f9231efc260876bb82ddc8de6e75a
proka89 <vukovicjovan89@gmail.com>
2018-01-15 21:19:47 +0100

    Commit 5
    Using WP_Query

---
+++ page-home.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+<div class="row">
+  <div class="col-xs-12">
+    <?php
+      $lastBlog = new WP_Query('type=post&posts_per_page=1');
+      if ($lastBlog->have_posts()):
+      while($lastBlog->have_posts()): $lastBlog->the_post(); ?>
+        <?php get_template_part('content', get_post_format()); ?>
+      <?php endwhile;
+      endif;
+      //Always use this function when using new WP_Query
+      wp_reset_postdata();
+     ?>
+  </div>
+  <div class="col-xs-12 col-sm-8">
+    <?php
+    if (have_posts()):
+    while(have_posts()): the_post(); ?>
+      <?php get_template_part('content', get_post_format()); ?>
+    <?php endwhile;
+    endif;
+
+    // Print other two posts without the first include_once
+    // Using $args variable to store array with parameters is equal to 'type=post&posts_per_page=2&offset=2'
+    $args = array(
+      'type' => 'post',
+      'posts_per_page' => 2,
+      'offset' => 2,
+    );
+    $lastBlog = new WP_Query($args);
+    if ($lastBlog->have_posts()):
+    while($lastBlog->have_posts()): $lastBlog->the_post(); ?>
+      <?php get_template_part('content', get_post_format()); ?>
+    <?php endwhile;
+    endif;
+    //Always use this function when using new WP_Query
+    wp_reset_postdata();
+     ?>
+     <!-- Print tutorials
+     posts_per_page=-1 sets number of posts per page to infinite, cat=8 is tutorials category id -->
+     <hr>
+     <?php
+     $lastBlog = new WP_Query('type=post&posts_per_page=-1&cat=8');
+     if ($lastBlog->have_posts()):
+     while($lastBlog->have_posts()): $lastBlog->the_post(); ?>
+       <?php get_template_part('content', get_post_format()); ?>
+     <?php endwhile;
+     endif;
+     //Always use this function when using new WP_Query
+     wp_reset_postdata();
+      ?>
+  </div>
+  <div class="col-xs-12 col-sm-4">
+    <?php get_sidebar(); ?>
+  </div>
+</div>
+<?php get_footer(); ?>
